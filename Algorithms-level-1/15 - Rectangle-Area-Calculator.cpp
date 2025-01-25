827369cc0f60f76d6b7c485671ac6c122ab09632
Dragonkuro <Hicham.bourezi@gmail.com>
2025-01-25 23:45:38 +0100

    A program that calculat a rectangle area

---
+++ Algorithms-level-1/15 - Rectangle-Area-Calculator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	float height, weight;
+	cout << "Enter the height of the rectangle: ";
+	cin >> height;
+	cout << "Enter the weight of the rectangle: ";
+	cin >> weight;
+	cout << "The area of the rectangle is: " << height * weight << endl;
+	return 0;
+}
