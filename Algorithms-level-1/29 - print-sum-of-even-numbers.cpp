091ec56d0d8b0e04d36045517fc8584ac18a23ee
Dragonkuro <Hicham.bourezi@gmail.com>
2025-01-28 22:39:51 +0100

    A program that calculate the sum of even numbers

---
+++ Algorithms-level-1/29 - print-sum-of-even-numbers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+void PrintNumbers(int number) {
+	int sum = 0;
+	while (number > 0) {
+		if (number % 2 == 0)
+			sum += number;
+		number--;
+	}
+
+	cout << "The sum is: " << sum << endl;
+}
+
+int main() {
+	int number;
+	cout << "Enter a number: ";
+	cin >> number;
+
+	PrintNumbers(number);
+	return 0;
+}
