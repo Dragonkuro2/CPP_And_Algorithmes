48191641c4290b3085c69fbbda448e57855bc8ce
Dragonkuro <Hicham.bourezi@gmail.com>
2025-01-29 13:35:17 +0100

    A very simple program that prints my name

---
+++ Structured_Solutions/Level_1/01-05/01-print_name.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+void PrintName(string name) {
+	cout << "\nYour name is: " << name << endl;
+}
+
+int main() { // program that prints the name we give it.
+
+	PrintName("Hicham");
+	
+	return 0;
+}
