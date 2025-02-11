205eed9ed10e2f8e513c0dadcf55febabf864f9e
Hicham Bourezi <Hicham.bourezi@gmail.com>
2025-02-11 15:59:17 +0100

    A program that print letter patterns

---
+++ Structured_Solutions/level_2/11-20/14-PrintLetterPatterns.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int ReadPositiveNumber(string message) {
+	int number;
+	do {
+		cout << message;
+		cin >> number;
+	} while (number < 0);
+
+	return number;
+}
+
+void PrintPatterns(int number) {
+	for (int i = number; i > 0; i--) {
+		for (int j = 0; j < i; j++) {
+			cout << (char)(i + 64);
+		}
+		cout << endl;
+	}
+}
+
+int main() {
+	PrintPatterns(ReadPositiveNumber("Enter a positive number: "));
+	return 0;
+}
