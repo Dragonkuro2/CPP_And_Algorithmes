f48b4af30fa8f830d79e0677ee5bb5fc349f3766
Hicham Bourezi <Hicham.bourezi@gmail.com>
2025-02-12 20:24:18 +0100

    A program that calculate the avirage of an array

---
+++ Structured_Solutions/level_2/21-30/27-AverageOfArray.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int const MAX_SIZE = 100;
+
+int ReadPositiveNumber(string message) {
+	int number;
+	do {
+		cout << endl << message;
+		cin >> number;
+	} while (number <= 0);
+	return number;
+}
+
+int RnadomNumberInRange(int From, int To) {
+	return rand() % (To - From + 1) + From;
+}
+
+void ReadTable(int array[MAX_SIZE], int &length) {
+	length = ReadPositiveNumber("Enter the size of the array: ");
+	for (int i = 0; i < length; i++) {
+		array[i] = RnadomNumberInRange(1, 100);
+	}
+}
+
+void PrintTable(int array[MAX_SIZE], int length) {
+	cout << "Array elements: ";
+	for (int i = 0; i < length; i++) {
+		cout << array[i] << " ";
+	}
+	cout << endl;
+}
+
+int SumOfArray(int array[MAX_SIZE], int length) {
+	int sum = 0;
+	for (int i = 0; i < length; i++) {
+		sum += array[i];
+	}
+	return sum;
+}
+
+float AverageOfArray(int array[MAX_SIZE], int length) {
+	return (float)SumOfArray(array, length) / length;
+}
+
+int main() {
+	int array[MAX_SIZE], length;
+	srand((unsigned)time(NULL));
+
+	ReadTable(array, length);
+	PrintTable(array, length);
+	cout << "the average of the array is: " << AverageOfArray(array, length) << endl;
+	return 0;
+}
