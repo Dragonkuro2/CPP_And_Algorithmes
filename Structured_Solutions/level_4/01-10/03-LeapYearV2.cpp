dbae7a519a2f413b44567346802a451854932801
Hicham Bourezi <Hicham.bourezi@gmail.com>
2025-03-05 23:58:43 +0000

    Updated version of checking for leap year

---
+++ Structured_Solutions/level_4/01-10/03-LeapYearV2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int ReadNumber(string message) {
+	int number;
+	cout << message;
+	cin >> number;
+	while (cin.fail()) {
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		cout << "Invalid Input!" << endl;
+		cout << message;
+		cin >> number;
+	}
+	return number;
+}
+
+int ReadPositiveNumber(string message) {
+	int number;
+	do {
+		number = ReadNumber(message);
+		if (number < 0)
+			cout << "\nEnter a positive number! please try again.\n" << endl;
+	} while (number < 0);
+	return number;
+}
+
+bool IsLeapYear(int year) {
+	return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+}
+
+int main() {
+	int Year = ReadPositiveNumber("Enter a positive number: ");
+
+	if (IsLeapYear(Year))
+		cout << "Yes, the year " << Year << " is a leap year." << endl;
+	else
+		cout << "No, the year " << Year << " isn't a leap year." << endl;
+
+	return 0;
+}
