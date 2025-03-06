5cea516d8acfaf8c3a3c7b9e3731dfbe47378573
Hicham Bourezi <Hicham.bourezi@gmail.com>
2025-03-06 15:25:44 +0000

    increase the date with N month

---
+++ Structured_Solutions/level_4/21-30/24-IncreaseXMonth.cpp
@@ -0,0 +1,174 @@
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+#include <string>
+
+using namespace std;
+
+struct stDate {
+	int year;
+	int month;
+	int day;
+};
+
+int ReadNumber(string message) {
+	int number;
+	cout << message;
+	cin >> number;
+	while (cin.fail()) {
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		cout << "Invalid Input!" << endl;
+		cout << message;
+		cin >> number;
+	}
+	return number;
+}
+
+int ReadPositiveNumber(string message) {
+	int number;
+	do {
+		number = ReadNumber(message);
+		if (number < 0)
+			cout << "\nEnter a positive number! please try again.\n" << endl;
+	} while (number < 0);
+	return number;
+}
+
+int ReadNumberInRange(string message, int From, int To) {
+	int number;
+	do {
+		number = ReadNumber(message);
+		if (number < From || number > To)
+			cout << "Invalid Input! Try Again.\n";
+	} while (number < From || number > To);
+	return number;
+}
+
+bool IsLeapYear(int year) {
+	return (year % 400 == 0) || (year % 4 == 0) && (year % 100 != 0);
+}
+
+int GetMonthDays(int year, int month) {
+	if (month < 1 || month > 12)
+		return 0;
+	int MonthDays[] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+	return (month == 2) ? IsLeapYear(year) ? 29 : 28 : MonthDays[month - 1];
+}
+
+int ReadYear() {
+	return ReadPositiveNumber("Enter a year: ");
+}
+
+int ReadMonth() {
+	return ReadNumberInRange("Enter a month: ", 1, 12);
+}
+
+int ReadDay(int year, int month) { // limit the user to enter just the number of days in that month
+	return ReadNumberInRange("Enter a day: ", 1, GetMonthDays(year, month));
+}
+
+stDate ReadDate(string message) {
+	stDate date;
+	cout << message << endl;
+	date.year = ReadYear();
+	date.month = ReadMonth();
+	date.day = ReadDay(date.year, date.month);
+	return date;
+}
+
+bool IsLastDayInMonth(stDate date) {
+	int MonthDays = GetMonthDays(date.year, date.month);
+	return MonthDays == date.day;
+}
+
+bool IsLastMonthInYear(int month) {
+	return month == 12;
+}
+
+stDate IncreaseDate(stDate date) {
+	if (IsLastDayInMonth(date)) {
+		date.day = 1;
+		if (IsLastMonthInYear(date.month)) {
+			date.month = 1;
+			date.year++;
+		}
+		else {
+			date.month++;
+		}
+	}
+	else {
+		date.day++;
+	}
+
+	return date;
+}
+
+stDate IncreaseXDay(stDate date, int DaysToIncrease) {
+	if (DaysToIncrease >= 0) { // check if the days to increase is a positve number
+		for (int i = 0; i < DaysToIncrease; i++)
+			date = IncreaseDate(date);
+	}
+
+	return date;
+}
+
+stDate IncreaseOneWeek(stDate date) {
+	return IncreaseXDay(date, 7); // we used the last function because we knows that a week is 7 days xD
+}
+
+stDate IncreaseXWeeks(stDate date, int NumberOfWeeks) {
+	if (NumberOfWeeks >= 0) {
+		for (int i = 0; i < NumberOfWeeks; i++)
+			date = IncreaseOneWeek(date);
+	}
+
+	return date;
+}
+
+stDate IncreaseOneMonth(stDate date) {
+	int MonthDays;
+
+	if (date.month == 12) {
+		date.year++;
+		date.month = 1;
+	} else
+		date.month++;
+
+	//get the days of the current month
+	MonthDays = GetMonthDays(date.year, date.month);
+	if (date.day > MonthDays) {
+		date.day = MonthDays; // if we have out of range we just set the last day on that month
+	}
+
+	
+	return date;
+}
+
+stDate IncreaseXMonth(stDate date, int NumberOfMonthToIncrease) {
+	if (NumberOfMonthToIncrease >= 0) {
+		for (int i = 0; i < NumberOfMonthToIncrease; i++) {
+			date = IncreaseOneMonth(date);
+		}
+	}
+	return date;
+}
+
+void PrintDate(string message, stDate date) {
+	cout << message << ": " << date.day << "/" << date.month << "/" << date.year << endl;
+}
+
+int main() {
+	stDate date;
+	int NumberOfMonthsToAdd;
+
+	date = ReadDate("Enter The Date: ");
+	NumberOfMonthsToAdd = ReadPositiveNumber("Enter the number of months to add: ");
+	date = IncreaseXMonth(date, NumberOfMonthsToAdd);
+	PrintDate("Date After Adding "+ to_string(NumberOfMonthsToAdd) + " Month: ", date);
+	
+	cout << "\nPress Enter to exit...";
+	cin.ignore(numeric_limits<streamsize>::max(), '\n');
+	cin.get();
+	return 0;
+}
