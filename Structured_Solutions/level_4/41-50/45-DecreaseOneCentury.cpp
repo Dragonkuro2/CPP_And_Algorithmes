7aba3c79a71433db5c72ce2748dcf2bd12175602
Hicham Bourezi <Hicham.bourezi@gmail.com>
2025-03-07 01:57:29 +0000

    decraese the date with one century

---
+++ Structured_Solutions/level_4/41-50/45-DecreaseOneCentury.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct stDate {
+	int year;
+	int month;
+	int day;
+};
+
+int ReadNumber(string message) {
+	int number;
+	cout << message;
+	cin >> number;
+	while (cin.fail()) {
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		cout << "Invalid Input!" << endl;
+		cout << message;
+		cin >> number;
+	}
+	return number;
+}
+
+int ReadPositiveNumber(string message) {
+	int number;
+	do {
+		number = ReadNumber(message);
+		if (number < 0)
+			cout << "\nEnter a positive number! please try again.\n" << endl;
+	} while (number < 0);
+	return number;
+}
+
+int ReadNumberInRange(string message, int From, int To) {
+	int number;
+	do {
+		number = ReadNumber(message);
+		if (number < From || number > To)
+			cout << "Invalid Input! Try Again.\n";
+	} while (number < From || number > To);
+	return number;
+}
+
+bool IsLeapYear(int year) {
+	return (year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0));
+}
+
+int GetMonthDays(int year, int month) {
+	if (month < 1 || month > 12)
+		return 0;
+	int MonthDays[] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+	return (month == 2) ? IsLeapYear(year) ? 29 : 28 : MonthDays[month - 1];
+}
+
+int ReadYear() {
+	return ReadPositiveNumber("Enter a year: ");
+}
+
+int ReadMonth() {
+	return ReadNumberInRange("Enter a month: ", 1, 12);
+}
+
+int ReadDay(int year, int month) { // limit the user to enter just the number of days in that month
+	return ReadNumberInRange("Enter a day: ", 1, GetMonthDays(year, month));
+}
+
+stDate ReadDate(string message) {
+	stDate date;
+	cout << message << endl;
+	date.year = ReadYear();
+	date.month = ReadMonth();
+	date.day = ReadDay(date.year, date.month);
+	return date;
+}
+
+stDate DecreaseXDecadeFaster(stDate date, int NumberOfDecades) {
+	if (NumberOfDecades > 0) {
+		date.year -= NumberOfDecades * 10;
+		// check feb month if it is 28 or 29 and correct it.
+		if ((date.month == 2) && (date.day > GetMonthDays(date.year, date.month)))
+			date.day = 28;
+	}
+
+	return date;
+}
+
+stDate DecreaseOneCentury(stDate date) {
+	// a century is 100 year, or 10 decades
+	return DecreaseXDecadeFaster(date, 10);
+}
+
+void PrintDate(string message, stDate date) {
+	cout << message << ": " << date.day << "/" << date.month << "/" << date.year << endl;
+}
+
+int main() {
+	stDate date;
+
+	date = ReadDate("Enter The Date: ");
+	date = DecreaseOneCentury(date);
+	PrintDate("Date After Decreasing One Century: ", date);
+	
+	cout << "\nPress Enter to exit...";
+	cin.ignore(numeric_limits<streamsize>::max(), '\n');
+	cin.get();
+	return 0;
+}
